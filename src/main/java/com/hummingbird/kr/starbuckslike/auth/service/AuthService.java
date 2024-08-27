51be1bbefd00b035f8ca4b6dc2343bf19ba86f0f
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-08-27 10:39:43 +0900

    :construction: making auth service

---
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.hummingbird.kr.starbuckslike.auth.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private String PWencryption(String password){
+        String EncodedPassword = "";
+        return EncodedPassword;
+    }
+}
