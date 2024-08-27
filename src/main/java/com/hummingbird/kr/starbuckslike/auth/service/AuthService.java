b768bc51aca5e8bd059fd966eebd9b235fd3592f
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-08-27 16:54:33 +0900

    :construction: create interface of AuthService

--- src/main/java/com/hummingbird/kr/starbuckslike/auth/service/AuthService.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/service/AuthService.java
@@ -1,12 +1,37 @@
 package com.hummingbird.kr.starbuckslike.auth.service;
 
-import org.springframework.stereotype.Service;
+import java.time.LocalDate;
 
-@Service
-public class AuthService {
+public interface AuthService {
 
-    private String PWencryption(String password){
-        String EncodedPassword = "";
-        return EncodedPassword;
-    }
+
+    String sendAuthChallenge(String email);
+
+    String sendAuthSMS(String phoneNumber);
+
+    void verifyAuthChallenge(String email, String authChallenge);
+
+    void verifyAuthSMS(String phoneNumber, String authSMS);
+
+
+    void registerMember(String loginID, String email, String password, String name, String phone, String Nickname, LocalDate birth);
+
+    void checkEmail(String email);
+
+    void checkId(String loginID);
+
+    void checkphone(String phone);
+
+
+    void login(String loginID, String password);
+
+    void logout();
+    
+
+    void OAuthLogin(String loginID, String password);
+
+    void OAuthRegister(String loginID, String email, String password, String name, String phone, String Nickname, LocalDate birth);
+
+
+    void restetPassword(String loginID, String email);
 }
