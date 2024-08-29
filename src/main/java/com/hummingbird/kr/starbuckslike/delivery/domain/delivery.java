488bc461a44f81541474bf95de76d52aa6389428
digetlyn <digetlyn@naver.com>
2024-08-29 15:33:06 +0900

    delivery 내용추가 및 수정중

--- src/main/java/com/hummingbird/kr/starbuckslike/delivery/domain/delivery.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/delivery/domain/delivery.java
@@ -1,7 +1,31 @@
 package com.hummingbird.kr.starbuckslike.delivery.domain;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "delivery")
 public class delivery {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_id")
+    private Long id;   //배송지 아이디
+
+    @Column(name = "altername")
+    private String alias; //배송지 별칭
+
+    @Column(name = "name")
+    private String name; //배송지 받는사람 이름
+
+    @Column(name = "address")
+    private String address; //배송지 기본주소 기입
+
+    @Column(name = "phone")
+    private int phone; //배송지연락처
+
+    private String userid; // 유저아이디
+
+ //   private String basic_flag;   // 기본주소지 등록할껀지 묻는 부분..
 
 }
