3fe5f9e55bc820eef78d34111a4ca150ed924164
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-02 17:01:14 +0900

    :construction: setting spring security 2
    still under construction

--- src/main/java/com/hummingbird/kr/starbuckslike/auth/config/SecurityConfig.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/config/SecurityConfig.java
@@ -10,32 +10,64 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final AuthService authService;
-    private final CustomAuthenticationProvider customAuthenticationProvider;
+    private final CustomAuthenticationProvider authenticationProvider;
 
     @Autowired
     public SecurityConfig(AuthService authService, CustomAuthenticationProvider customAuthenticationProvider) {
         this.authService = authService;
-        this.customAuthenticationProvider = customAuthenticationProvider;
+        this.authenticationProvider = customAuthenticationProvider;
+    }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setExposedHeaders(List.of("Authorization"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-                .antMatchers("/api/members/register").permitAll()
-                .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests
+                                .requestMatchers(
+                                        "/api/v1/auth/**",
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilter(corsFilter());
         return http.build();
     }
 
@@ -49,8 +81,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(13);
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("/api/members/register");
-    }
 }
