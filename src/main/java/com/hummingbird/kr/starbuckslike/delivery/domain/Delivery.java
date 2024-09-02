27f7897755a2e1b756ee488b87f56bed0a8c5875
digetlyn <digetlyn@naver.com>
2024-09-02 15:00:33 +0900

    caches/8.8/fileContent/fileContent.lock 에러

--- src/main/java/com/hummingbird/kr/starbuckslike/delivery/domain/Delivery.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/delivery/domain/Delivery.java
@@ -2,17 +2,25 @@ package com.hummingbird.kr.starbuckslike.delivery.domain;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "delivery")
-public class delivery {
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Delivery")
+public class Delivery {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "delivery_id")
     private Long id;   //배송지 아이디
 
-    @Column(name = "altername")
+    @Column(name = "alias")
     private String alias; //배송지 별칭
 
     @Column(name = "name")
@@ -21,13 +29,8 @@ public class delivery {
     @Column(name = "address")
     private String address; //배송지 기본주소 기입
 
-    @Column(name = "phone")
+    @Column(name = "phone")  //@Column(name = " ") 필드와 매핑할 컬럼의 이름을 설정, 기본값 객체의 필드이름
     private int phone; //배송지연락처
-
-
-    @Column(name = "phone2")
-    private int phone2; //배송지연락처
-
 
     private String userid; // 유저아이디
 
--- src/main/java/com/hummingbird/kr/starbuckslike/delivery/dto/DeliveryDto.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/delivery/dto/DeliveryDto.java
@@ -1,4 +1,23 @@
 package com.hummingbird.kr.starbuckslike.delivery.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
 public class DeliveryDto {
+
+
+    private Long id;
+    private String alias;
+    private String name;
+    private String address;
+    private int phone;
+    private String userid;
+    //11231
+    //   private String basic_flag;
 }
