a29ad06c2749d7562ef51d7e8c173823bd8571d4
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-10 11:05:15 +0900

    :construction: apply Garnet(Redis written by .NET lang.) Service for Logout requested Token

---
+++ src/main/java/com/hummingbird/kr/starbuckslike/config/GarnetConfig.java
@@ -0,0 +1,42 @@
+package com.hummingbird.kr.starbuckslike.config;
+
+// this is Configuration Class of Spring which control Garnet Connection
+// Garnet is pork of Redis which created MS, developed in .NET 8.0, C#
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Date;
+
+@Configuration
+public class GarnetConfig {
+    @Value("${spring.garnet.host}")
+    private String redisHost;
+
+    @Value("${spring.garnet.port}")
+    private int redisPort;
+    
+    //Redis Connection Factory 객체 생성 : Redis - Spring App 연결 생성기입니다.
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        return factory;
+    }
+    
+    // RedisTemplate는 JPA Repository를 대신하는 Redis에 넘겨줄 객체를 정의하는 방식입니다.
+    // RedisTemplate<key type, value type>의 구조를 활용하고요
+    // Redis를 쓸 서비스에서 private final로 RedisTemplate을 가져온 다음
+    // RedisTemplate.opsForValue().set(key, value, ttl, TimeUnit.SECONDS) 형식으로 값 정의
+    // RedisTemplate.opsForValue().get(key) 를 통해 값 가져오기
+    @Bean
+    public RedisTemplate<String, Date> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Date> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
