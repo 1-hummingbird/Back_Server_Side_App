8bf9c8ec3db503f27faf062ace9720d431b6ba30
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-11 15:54:02 +0900

    :sparkles: BaseExceptionHandler Implement
    BaseResponse Entity Created
    BaseResponseStatus Entity Create
    (need improvement to fit our proj.)
    AsyncExceptionHandler Created
    BaseException Entity Created
    BaseExceptionHandler Created
    BaseExceptionHandlerFilter Created

---
+++ src/main/java/com/hummingbird/kr/starbuckslike/common/Exception/BaseExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.hummingbird.kr.starbuckslike.common.Exception;
+
+import com.hummingbird.kr.starbuckslike.common.entity.BaseResponse;
+import com.hummingbird.kr.starbuckslike.common.entity.BaseResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class BaseExceptionHandler {
+
+    /**
+     * 발생한 예외 처리
+     */
+
+    @ExceptionHandler(BaseException.class)
+    protected ResponseEntity<BaseResponse<Void>> BaseError(BaseException e) {
+        BaseResponse<Void> response = new BaseResponse<>(e.getStatus());
+        log.error("BaseException -> {}({})", e.getStatus(), e.getStatus().getMessage(), e);
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+    /**
+     * security 인증 에러
+     * 아이디가 없거나 비밀번호가 틀린 경우 AuthenticationManager 에서 발생
+     *
+     * @return FAILED_TO_LOGIN 에러 response
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    protected ResponseEntity<BaseResponse<Void>> handleBadCredentialsException(BadCredentialsException e) {
+        BaseResponse<Void> response = new BaseResponse<>(BaseResponseStatus.FAILED_TO_LOGIN);
+        log.error("BadCredentialsException: ", e);
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<BaseResponse<Void>> RuntimeError(RuntimeException e) {
+        BaseResponse<Void> response = new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR);
+        log.error("RuntimeException: ", e);
+        for (StackTraceElement s : e.getStackTrace()) {
+            System.out.println(s);
+        }
+        return new ResponseEntity<>(response, response.httpStatus());
+    }
+
+}
---
+++ src/main/java/com/hummingbird/kr/starbuckslike/common/entity/BaseResponse.java
@@ -0,0 +1,40 @@
+package com.hummingbird.kr.starbuckslike.common.entity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import static com.hummingbird.kr.starbuckslike.common.entity.BaseResponseStatus.SUCCESS;
+
+public record BaseResponse<T>(HttpStatusCode httpStatus, Boolean isSuccess, String message, int code, T result) {
+
+    /**
+     * 필요값 : Http상태코드, 성공여부, 메시지, 에러코드, 결과값
+     * 1. Return 객체가 필요한 경우 -> 성공
+     * 2. Return 객체가 필요 없는 경우 -> 성공
+     * 3. 요청에 실패한 경우
+     */
+
+    /**
+     * 1. Return 객체가 필요한 경우 -> 성공
+     * @param result
+     */
+    public BaseResponse(T result) {
+        this(HttpStatus.OK, true, SUCCESS.getMessage(), SUCCESS.getCode(), result);
+    }
+
+    /**
+     * 2. Return 객체가 필요 없는 경우 -> 성공
+     */
+
+    public BaseResponse() {
+        this(HttpStatus.OK, true, SUCCESS.getMessage(), SUCCESS.getCode(), null);
+    }
+
+    /**
+     * 3. 요청에 실패한 경우
+     * @param status
+     */
+    public BaseResponse(BaseResponseStatus status) {
+        this(status.getHttpStatusCode(), false, status.getMessage(), status.getCode(), null);
+    }
+
+}
