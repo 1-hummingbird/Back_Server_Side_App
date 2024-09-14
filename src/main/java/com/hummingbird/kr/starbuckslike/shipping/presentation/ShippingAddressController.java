8dd8998fb45e6be0b08a69f0bcd12de2397a260f
dayday <digetlyn@naver.com>
2024-09-14 23:09:04 +0900

    Controller 불필요한 단어 삭제

--- src/main/java/com/hummingbird/kr/starbuckslike/shipping/presentation/ShippingAddressController.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/shipping/presentation/ShippingAddressController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor   //생성자를 만들어주는 역할.11
+@RequiredArgsConstructor   //생성자를 만들어주는 역할.
 @RestController
 @RequestMapping("/api/v1/shipping-address")
 public class ShippingAddressController {
