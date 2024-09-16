45528482eed583c9880bc89200ea4ec213b9a7b7
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-16 12:34:39 +0900

    :bug: fix member controller swagger setting
    need's to login hint annotation added

--- src/main/java/com/hummingbird/kr/starbuckslike/member/application/MemberServiceImpl.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/member/application/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hummingbird.kr.starbuckslike.member.application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.hummingbird.kr.starbuckslike.member.infrastructrue.MemberRepository;
@@ -9,7 +10,7 @@ import com.hummingbird.kr.starbuckslike.member.dto.in.*;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-
+@Slf4j
 @Service
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
@@ -25,6 +26,7 @@ public class MemberServiceImpl implements MemberService {
         LocalDateTime createdAt = member.getCreatedAt();
         LocalDateTime currentTime = LocalDateTime.now();
         long daysBetween = ChronoUnit.DAYS.between(createdAt, currentTime);
+        log.info("passage daysBetween: {}", daysBetween);
         return new PassageResponseDTO(member.getMemberUID(), daysBetween);
     }
 }
--- src/main/java/com/hummingbird/kr/starbuckslike/member/presentation/MemberController.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/member/presentation/MemberController.java
@@ -4,6 +4,9 @@ import com.hummingbird.kr.starbuckslike.member.application.MemberService;
 import com.hummingbird.kr.starbuckslike.member.vo.out.*;
 import com.hummingbird.kr.starbuckslike.member.vo.in.*;
 import com.hummingbird.kr.starbuckslike.member.dto.out.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/member")
 @ComponentScan(basePackages = {"com.hummingbird.kr.starbuckslike.member"})
@@ -23,7 +27,9 @@ public class MemberController {
     }
 
     @PostMapping("passage")
+    @Operation(security = @SecurityRequirement(name = "Bearer Auth"))
     public PassageResponseVO passage(@RequestBody PassageRequestVO passageRequestVO) {
+        log.info("passage memberUID: {}", passageRequestVO.getMemberUID());
         PassageResponseDTO passageResponseDTO = memberService.passage(passageRequestVO.toDTO());
         return passageResponseDTO.toVO();
     }
