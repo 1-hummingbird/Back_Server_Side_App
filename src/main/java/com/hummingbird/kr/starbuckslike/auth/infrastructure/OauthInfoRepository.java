48a357e7204406f845bd6ecb6db845a98192efce
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-16 20:44:28 +0900

    :ambulance: fix mistyping

--- src/main/java/com/hummingbird/kr/starbuckslike/auth/infrastructure/OauthInfoRepository.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/infrastructure/OauthInfoRepository.java
@@ -4,7 +4,7 @@ import com.hummingbird.kr.starbuckslike.auth.domain.OauthInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OauthInfoRepository extends JpaRepository<OauthInfo, Long> {
-    OauthInfo findByOauthIDAndOauthType();
-    OauthInfo findBymemberUID();
+    OauthInfo findByOauthIDAndOauthType(String OauthID, String OauthType);
+    OauthInfo findBymemberUID(String memberUID);
 
 }
