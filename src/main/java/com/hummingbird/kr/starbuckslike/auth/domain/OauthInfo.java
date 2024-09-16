7366cfd18ec1a4e383d62891cf2447946358da67
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-16 20:49:07 +0900

    :bug: fix Oauthinfo Entity and Repository

--- src/main/java/com/hummingbird/kr/starbuckslike/auth/domain/OauthInfo.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/domain/OauthInfo.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "oauth_info")
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OauthInfo {
     
     @Id
--- src/main/java/com/hummingbird/kr/starbuckslike/auth/infrastructure/OauthInfoRepository.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/infrastructure/OauthInfoRepository.java
@@ -1,10 +1,21 @@
 package com.hummingbird.kr.starbuckslike.auth.infrastructure;
 
 import com.hummingbird.kr.starbuckslike.auth.domain.OauthInfo;
+import com.hummingbird.kr.starbuckslike.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
 
 public interface OauthInfoRepository extends JpaRepository<OauthInfo, Long> {
-    OauthInfo findByOauthIDAndOauthType(String OauthID, String OauthType);
-    OauthInfo findBymemberUID(String memberUID);
+
+    @Query("SELECT m FROM OauthInfo m WHERE m.oauthID = :oauthID AND m.oauthType = :oauthType")
+    Optional<OauthInfo> findByOauthIDAndOauthType(@Param("oauthID") String oauthID, @Param("oauthType") String oauthType);
+
+    @Query("SELECT m FROM OauthInfo m WHERE m.MemberUID = :MemberUID")
+    List<OauthInfo> findBymemberUID(@Param("MemberUID")String memberUID);
 
 }
