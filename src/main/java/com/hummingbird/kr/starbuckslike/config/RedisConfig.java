9981c30ef666481282cff04ccc7fc3ab0af1a507
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-09-20 12:13:23 +0900

    :rotating_light: Update RedisConfig.java

--- src/main/java/com/hummingbird/kr/starbuckslike/config/RedisConfig.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/config/RedisConfig.java
@@ -30,18 +30,12 @@ public class RedisConfig  {
     @Value("${spring.data.redis.port}")
     private int port;
 
-//     @Value("${spring.data.redis.password}")
-//     private String password;
-
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
         redisConfiguration.setHostName(host);
         redisConfiguration.setPort(port);
-        redisConfiguration.setPassword(password);
-
         return new LettuceConnectionFactory(redisConfiguration);
-
     }
 
     @Bean
