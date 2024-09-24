77d95c58edf859c199b810058858f2a1b243da43
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-09-24 15:20:57 +0900

    :bug: Fix bug when check duplication(login id, phone, email)

--- src/main/java/com/hummingbird/kr/starbuckslike/auth/vo/in/CheckEmailRequestVO.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/vo/in/CheckEmailRequestVO.java
@@ -1,12 +1,17 @@
 package com.hummingbird.kr.starbuckslike.auth.vo.in;
 
 import com.hummingbird.kr.starbuckslike.auth.dto.in.CheckEmailRequestDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CheckEmailRequestVO {
     private String email;
+
 
     public CheckEmailRequestDTO toDTO() {
         return CheckEmailRequestDTO.builder()
--- src/main/java/com/hummingbird/kr/starbuckslike/auth/vo/in/CheckLoginIDRequestVO.java
+++ src/main/java/com/hummingbird/kr/starbuckslike/auth/vo/in/CheckLoginIDRequestVO.java
@@ -1,15 +1,15 @@
 package com.hummingbird.kr.starbuckslike.auth.vo.in;
 
 import com.hummingbird.kr.starbuckslike.auth.dto.in.CheckLoginIDRequestDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CheckLoginIDRequestVO {
     private String loginID;
-
-    public CheckLoginIDRequestVO(String loginID) {
-        this.loginID = loginID;
-    }
 
     public CheckLoginIDRequestDTO toDTO() {
         return new CheckLoginIDRequestDTO(this.loginID);
